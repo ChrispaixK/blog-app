18eb92bde60ab896996f04901153b0e4e742b65a
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-10-18 23:19:35 +0200

    Add User migration

--- db/migrate/20221018211619_create_user.rb
+++ db/migrate/20221018211619_create_user.rb
@@ -1,8 +1,8 @@
-class CreateTableUser < ActiveRecord::Migration[7.0]
+class CreateUser < ActiveRecord::Migration[7.0]
   def change
-    create_table :table_users do |t|
+    create_table :users do |t|
       t.string :name
-      t.text :photo
+      t.string :photo
       t.text :bio
       t.integer :posts_counter
       t.timestamps
