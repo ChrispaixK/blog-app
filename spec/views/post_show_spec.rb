bec46f3d93fd2d69af37f7e7130f99e467a851a4
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-01 10:28:29 +0100

    add test for post show

--- spec/views/post_show_spec.rb
+++ spec/views/post_show_spec.rb
@@ -1,11 +1,11 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Users and user page contents', type: :feature do
-#   describe 'Check the post show page' do
-#     it 'Check the posts Page title' do
-#         visit '/users/1/posts/1'
-#         expect(page).to have_content('Specific post')
-#     end
+RSpec.describe 'Users and user page contents', type: :feature do
+  describe 'Check the post show page' do
+    it 'Check the posts Page title' do
+        visit '/users/1/posts/1'
+        expect(page).to have_content('Specific post')
+    end
 
 #     it 'Check the posts author' do
 #         visit '/users/1/posts/1'
