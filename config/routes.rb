3668f46141858e100e97a402fcdcf4ee78ae7f32
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-02 17:53:18 +0100

    add the devise to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-# Rails.application.routes.draw do
-#   devise_for :users, sign_out_via: %i[get post]
-#   resources :users, only: %i[index show] do
-#     resources :posts, only: %i[index show create new] do
-#       resources :comments, only: %i[create new]
-#       resources :likes, only: [:create]
-#     end
-#   end
-#   root to: 'users#index'
-# end
+Rails.application.routes.draw do
+  devise_for :users, sign_out_via: %i[get post]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show create new] do
+      resources :comments, only: %i[create new]
+      resources :likes, only: [:create]
+    end
+  end
+  root to: 'users#index'
+end
